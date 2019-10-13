39fbf8657a87e4b978f0b3aa7dac18f833e40f16
Shailesh Chandra <shailesh.chandra@mytrusteddr.com>
2019-10-13 17:32:36 +0530

    renamed application class & add metrics method

--- src/main/java/com/example/actuatorservice/ActuatorApplication.java
+++ src/main/java/com/example/actuatorservice/ActuatorApplication.java
@@ -2,53 +2,44 @@ package com.example.actuatorservice;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.boot.actuate.metrics.MetricsEndpoint.AvailableTag;
 import org.springframework.boot.actuate.metrics.MetricsEndpoint.ListNamesResponse;
 import org.springframework.boot.actuate.metrics.MetricsEndpoint.MetricResponse;
 import org.springframework.boot.actuate.metrics.MetricsEndpoint.Sample;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Controller
-public class HelloWorldController {
-
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+@SpringBootApplication
+public class ActuatorApplication {
 
 	@Autowired
 	private MetricsEndpoint metricsEndpoint;
 
-	@GetMapping("/hello-world")
-	@ResponseBody
-	public Greeting sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorApplication.class, args);
 	}
 
-	@GetMapping("/printmetricsmaxmem")
-	@ResponseBody
-	public MetricResponse printJavaMaxMemMetrics() {
+	@PostConstruct
+	public void inokeMetrics() {
 
 		ListNamesResponse listNames = metricsEndpoint.listNames();
 
-		listNames.getNames().stream().forEach(System.out::println);
+		listNames.getNames().stream().forEach(System.out::println);//NOSONAR
 
 		MetricResponse metric = metricsEndpoint.metric("jvm.memory.max", new ArrayList<>());
 		System.out.println("metric (jvm.memory.max) ->" + metric);
 
 		List<AvailableTag> availableTags = metric.getAvailableTags();
 
-		availableTags.forEach(tag -> System.out.println(tag.getTag() + " : " + tag.getValues()));
+		availableTags.forEach(tag -> System.out.println(tag.getTag() + " : " + tag.getValues()));//NOSONAR
 
 		List<Sample> measurements = metric.getMeasurements();
 		measurements.forEach(sample -> System.out.println(sample.getStatistic() + " : " + sample.getValue()));
-		
-		return metric;
 
 	}
 
